409b4d5895116e6d0cf75cef5c2d52f0d19c637b
anuraggargLTI <anurag_garg@live.com>
2023-01-25 15:14:39 -0500

    updated symbol data file

--- symbol_data.py
+++ symbol_data.py
@@ -4,6 +4,7 @@ from fire import Fire
 
 di.initialize_symbol_master()
 di.initialize_symbol_details()
+
 def search_by_symbol(symbol):
     engine = di.return_engine_handler()
     search_by_symbol_query = f"""
@@ -21,7 +22,11 @@ def get_historical_data(symbol):
     symbol_details_df = pd.read_sql_query(get_historical_data_query,con=engine)
     print(symbol_details_df.head())
 
-if __name__ == "__main__":
-    Fire(get_historical_data)
+def get_all_tickers():
+    result_df=di.return_symbol_master_handler()
+    return result_df['Symbol'].iloc[0:10]
+
+
+
 
 
