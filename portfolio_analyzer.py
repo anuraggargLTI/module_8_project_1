5bdebb9765b28d2d97eb22267e9cd3de09428925
anuraggargLTI <anurag_garg@live.com>
2023-01-25 16:24:29 -0500

    main program to run WIP

--- portfolio_analyzer.py
+++ portfolio_analyzer.py
@@ -5,7 +5,11 @@ import portfolio_data as pfd
 import symbol_data as sd
 
 def show_task_menu():
-    menu_choice = questionary.select("Welcome Guest , what do you want to do now?", choices=['See the price of a ticker','See details of a ticker']).ask()
+    menu_choice = questionary.select("Welcome Guest , what do you want to do now?", choices=['See the price of a ticker',
+                                                                                             'See details of a ticker',
+                                                                                             'See the performance of pre-built portfolios',
+                                                                                             'Build a portfolio',
+                                                                                             'Compare portfolios']).ask()
     if menu_choice == 'See the price of a ticker':
         ticker_list = sd.get_all_tickers() 
         ticker_selected = questionary.select("Welcome Guest , here are few tickers for you to select from :", choices=ticker_list).ask()
