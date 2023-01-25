63c72ca70b0f6883f99fa6a076c0bb442d851b35
anuraggargLTI <64598470+anuraggargLTI@users.noreply.github.com>
2023-01-24 21:05:24 -0500

    Add files via upload

---
+++ symbol_data.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import db_initializer as di
+from fire import Fire
+
+di.initialize_symbol_master()
+di.initialize_symbol_details()
+def search_by_symbol(symbol):
+    engine = di.return_engine_handler()
+    search_by_symbol_query = f"""
+        SELECT * from SYMBOL_MASTER
+        WHERE symbol like '%{symbol}%'
+    """
+    symbol_master_df = pd.read_sql_query(search_by_symbol_query,con=engine)
+    print(symbol_master_df.head())
+
+def get_historical_data(symbol):
+    engine = di.return_engine_handler()
+    get_historical_data_query = f"""
+        SELECT * from SYMBOL_DETAILS_{symbol}
+    """
+    symbol_details_df = pd.read_sql_query(get_historical_data_query,con=engine)
+    print(symbol_details_df.head())
+
+if __name__ == "__main__":
+    Fire(get_historical_data)
+
+
