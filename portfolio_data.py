9872442202fd9d6d9b9c53daabcfdf87ff154ea4
trentransom <116039236+trentransom@users.noreply.github.com>
2023-01-31 22:07:16 -0500

    Changing all column names to lower case. moving symbol data into portfolio_data

--- portfolio_data.py
+++ portfolio_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import db_initializer as di
-from fire import Fire
+
 
 di.initialize_symbol_master()
 di.initialize_symbol_details()
@@ -23,7 +23,7 @@ def get_portfolio_historical_data(symbol_list):
         df_dict[symbol_name].index = pd.to_datetime(df_dict[symbol_name].index, infer_datetime_format = True)
         start_counter += 1
     symbol_details_df = pd.concat(df_dict.values(), axis=1, join = 'inner', keys=symbol_list)
-    print(symbol_details_df)
+    return symbol_details_df
 
 def get_portfolio_historical_close_data(symbol_list):
    
@@ -48,3 +48,33 @@ def get_portfolio_historical_close_data(symbol_list):
         start_counter += 1
     symbol_details_df = pd.concat(df_dict.values(), axis=1, join = 'inner', keys=symbol_list)
     return symbol_details_df
+
+def search_by_symbol(symbol):
+    engine = di.return_engine_handler()
+    search_by_symbol_query = f"""
+        SELECT * from SYMBOL_MASTER
+        WHERE symbol like '%{symbol}%'
+    """
+    return pd.read_sql_query(search_by_symbol_query,con=engine).drop(columns='index')
+    
+
+def get_historical_data(symbol):
+    engine = di.return_engine_handler()
+    get_historical_data_query = f"""
+        SELECT * from SYMBOL_DETAILS_{symbol}
+    """
+    
+    stock_df =  pd.read_sql_query(get_historical_data_query,con=engine)
+    if 'date' in stock_df.columns:
+        stock_df.index = stock_df['date']
+    if 'Date' in stock_df.columns:
+        stock_df.index = stock_df['Date']
+    stock_df.index = pd.to_datetime(stock_df.index, infer_datetime_format=True)
+    return stock_df
+
+def get_all_tickers():
+    result_df=di.return_symbol_master_handler()
+    return result_df['Symbol']
+
+def get_ticker_price(symbol):
+    return symbol
